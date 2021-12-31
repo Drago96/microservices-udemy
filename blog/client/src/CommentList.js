6a50c2fea203fb929665dd49a4ca451510463733
dproychev <dproychev@teladoc.com>
2021-12-31 17:40:43 +0200

    Add comment moderation

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,7 +1,11 @@
 const CommentList = ({ comments }) => (
   <ul>
     {comments.map((comment) => (
-      <li key={comment.id}>{comment.content}</li>
+      <li key={comment.id}>
+        {comment.status === "approved" && comment.content}
+        {comment.status === "pending" && "This comment is awaiting moderation"}
+        {comment.status === "rejected" && "This comment has been rejected"}
+      </li>
     ))}
   </ul>
 );
--- blog/query/index.js
+++ blog/query/index.js
@@ -24,13 +24,21 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, status, postId } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
-  console.log(posts);
+  if (type === "CommentUpdated") {
+    const { id, content, status, postId } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+    comment.content = content;
+  }
 
   res.status(204).send();
 });
