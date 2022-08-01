dad272c595195d22f319795727bcdcbd00e9e4f3
dproychev <dproychev@teladoc.com>
2022-08-01 16:16:39 +0300

    Add additional stan options

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -16,7 +16,11 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("listener-service");
 
   const subscription = stan.subscribe(
     "ticket:created",
