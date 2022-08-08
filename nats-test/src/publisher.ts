53a31999f0573f2947e481a8329034255f8a0bd3
dproychev <dproychev@teladoc.com>
2022-08-09 00:37:02 +0300

    Add reusable logic for nats listeners and publishers

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,21 +1,19 @@
 import nats from "node-nats-streaming";
 
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
 console.clear();
 
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
+  await new TicketCreatedPublisher(stan).publish({
     id: "123",
     title: "concert",
     price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
   });
 });
